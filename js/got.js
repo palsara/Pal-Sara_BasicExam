354f2ad76a09a3f8db1cbb8f39f8e78bc9e4da37
Pál Sára <pal.sara.ajandek@gmail.com>
2019-07-19 16:32:42 +0200

    added characters, sorted them, ready for onclick events

---
+++ js/got.js
@@ -0,0 +1,54 @@
+const Characters = {
+  characters: [],
+  init() {
+    this.findAll();
+  },
+  setUserData(userData) {
+    this.characters = JSON.parse(userData); // átalakítja a JSON filet. Innentől van feltöltve adatokkal a data
+    console.log(this.characters);
+    this.showAll();
+  },
+  findAll() {
+    const request = new XMLHttpRequest();
+    request.onload = () => { // onload eseményfigyelő
+      this.setUserData(request.responseText);
+    };
+    request.onerror = () => { // ha nem tölt be, alertet dob-NEM HASZNÁLUNK ALERTET
+      alert('Hiba a fájl betöltésekor');
+    };
+    request.open('GET', '/json/got.json');
+    request.send();
+  },
+  showAll() {
+    this.sortByName();
+    const divBig = document.querySelector('.div--big');
+    divBig.innerHTML = '';
+    for (let i = 0; i < this.characters.length; i += 1) {
+      if (!this.characters[i].dead) {
+        divBig.innerHTML += `<div class="div--characters" >
+        <div><img src="${this.characters[i].portrait}" alt="${this.characters[i].name}" data-name="${this.characters[i].name}" onclick="Characters.showMore();"></div>
+        <div data-name="${this.characters[i].name}" onclick="Characters.showMore();">${this.characters[i].name}</div>
+        </div>`;
+      }
+    }
+  },
+  sortByName() {
+    this.characters.sort((a, b) => {
+      const nameA = a.name.toUpperCase(); // nagybetűk és kisbetűk elhagyása
+      const nameB = b.name.toUpperCase(); // nagybetűk és kisbetűk elhagyása
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
+  },
+  showMore() {
+    const divName = event.target;
+    const divDataName = divName.getAttribute('data-name');
+    console.log(divDataName);
+  },
+};
+Characters.init();
